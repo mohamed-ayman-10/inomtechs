5dd26cac4cd91c27eed7a3e52e03480ecfc6dba7
mohamed-ayman-10 <mohamed-ayman-10>
2023-01-27 15:51:56 +0200

    first

---
+++ resources/views/admin/counter/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin.master')
+
+@section('content')
+
+    <!-- Include Alert Blade -->
+    @include('admin.alert.alert')
+
+    <!-- Form row -->
+    <div class="row">
+        <div class="col-xl-12 box-margin height-card">
+            <div class="card card-body">
+                <div class="d-md-flex justify-content-between align-items-center mb-20">
+                    <h4 class="card-title">{{ __('content.edit_counter') }}</h4>
+                    <div>
+                        <a href="{{ url()->previous() }}" class="btn btn-primary"><i class="fas fa-angle-left"></i> {{ __('content.back') }}</a>
+                    </div>
+                </div>
+            @if ($demo_mode == "on")
+                <!-- Include Alert Blade -->
+                    @include('admin.demo_mode.demo-mode')
+                @else
+                    <form action="{{ route('counter.update', $counter->id) }}" method="POST">
+                        @method('PUT')
+                        @csrf
+                        @endif
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="timer">{{ __('content.timer') }} <span class="text-red">*</span></label>
+                                    <input type="number" name="timer" class="form-control" id="timer" value="{{ $counter->timer }}" required>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="title">{{ __('content.title') }} <span class="text-red">*</span></label>
+                                    <input type="text" name="title" class="form-control" id="title" value="{{ $counter->title }}" required>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="order">{{ __('content.order') }}</label>
+                                    <input type="number" name="order" class="form-control" id="order" value="{{ $counter->order }}" required>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <small class="form-text text-muted">{{ __('content.required_fields') }}</small>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary mr-2">{{ __('content.submit') }}</button>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+        </div>
+    </div>
+    <!-- end row -->
+
+@endsection
